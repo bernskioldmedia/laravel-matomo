ec095357a05c1076bb4e6c9785b60e9cd3d330f8
Erik Bernskiold <erik@bernskioldmedia.com>
2022-08-06 16:27:38 +0200

    Cleanup
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,10 +11,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'BernskioldMedia\\LaravelMatomo\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
@@ -22,15 +18,5 @@ class TestCase extends Orchestra
         return [
             LaravelMatomoServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-matomo_table.php.stub';
-        $migration->up();
-        */
     }
 }
